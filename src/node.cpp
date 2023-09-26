e4631e5c28ff8e86f826d296bda5d3b9d4858be9
Stew-000-1-0-011 <stew00010011@gmail.com>
2023-09-26 19:35:29 +0900

    足回りのノード名を追加

--- src/node.cpp
+++ src/node.cpp
@@ -10,7 +10,7 @@ int main(int argc, char * argv[])
 
 	std::cout << "initilalizing..." << std::endl;
 
-	auto joy_to_indster = std::make_shared<nhk2024::joy_to_independent_steering_n::node::Node>("joy_to_independent_steering_n", options);
+	auto joy_to_indster = std::make_shared<nhk2024::joy_to_independent_steering_n::node::Node>("joy_to_independent_steering_n", "independent_steering_n", options);
 	exec.add_node(joy_to_indster);
 	
 	std::cout << "start spinning." << std::endl;
